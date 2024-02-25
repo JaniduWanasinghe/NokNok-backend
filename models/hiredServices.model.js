b716b018923d53ab94eaf9b0345d3cf8d0469c5f
JaniduWanasinghe <chamodjwanasinghe@gmail.com>
2024-02-25 20:09:32 +0530

    feat(hiredService model)

---
+++ models/hiredServices.model.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const HiredServiceSchema = new Schema(
+  {
+    serviceId: {
+      type: String,
+      required: true,
+    },
+    providerId: {
+        type: String,
+        required: true,
+      },
+      userId: {
+        type: String,
+        required: true,
+      },
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    noHours: {
+      type: String,
+      required: true,
+    },
+
+    Total: {
+      type: String,
+      required: true,
+    },
+    review: {
+      type: Number,
+      required: true,
+    },
+    
+    status: {
+      type: String,
+      default: 0,
+    }
+    ,
+    
+    review: {
+      type: String,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("HiredService", HiredServiceSchema);
