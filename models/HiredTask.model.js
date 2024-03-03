862c817746725dc8b734f8f221a8267fd8255a9f
Induranga21 <indurangaharitha@gmail.com>
2024-03-04 02:36:27 +0530

    feat(hired tasks model)

---
+++ models/HiredTask.model.js
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+
+const HiredTaskSchema = new Schema(
+  {
+    sellerId: {
+      type: String,
+      required: true,
+    },
+    buyerId: {
+        type: String,
+        required: true,
+      },
+    title: {
+      type: String,
+      required: true,
+    },
+    note: {
+      type: String,
+      required: false,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    totalStars: {
+      type: Number,
+      default: 0,
+    },
+    reviews: {
+      type: String,
+      required: false,
+    },
+
+    serviceId: {
+      type: String,
+      required: true,
+    },
+    rate:{
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    cover: {
+      type: String,
+      required: true,
+    },
+    hours: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: "pending",
+    },
+    payment: {
+      type: String,
+      default:"Pending",
+    }
+
+ 
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("HiredTask", HiredTaskSchema);
